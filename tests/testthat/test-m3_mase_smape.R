74ec5fb63681aa99d93cde7fc7993b20e25a3d71
Faluhelyi <igor.faluhelyi@outlook.com>
2024-11-26 18:49:10 -0300

    refactor: align scripts with lintr standards for improved code quality inside forecTheta test framework

--- tests/testthat/test-m3_mase_smape.R
+++ tests/testthat/test-m3_mase_smape.R
@@ -2,11 +2,6 @@ library(Mcomp)
 library(parallel)
 library(doParallel)
 library(iterators)
-#data(M3)  # load M3 data
-#devtools::load_all(".")  # load local forecTheta
-
-
-#inicio <- Sys.time()
 
 # cluster config
 cl <- makeCluster(detectCores(logical = FALSE))
@@ -16,49 +11,39 @@ registerDoParallel(cl)
 clusterEvalQ(cl, devtools::load_all("."))
 clusterExport(cl, c("dotm", "dstm", "otm", "stm"))
 
-out_dotm <- foreach(i = 1:3003, 
-               .packages = c("foreach", "forecast", "Mcomp")) %dopar% {
-                 x <- M3[[i]]$x
-                 h <- M3[[i]]$h
-                 
-                 f <- dotm(y = x, h = h)
-                 
-                 #f
-               }
+out_dotm <- foreach(
+  i = 1:3003, .packages = c("foreach", "forecast", "Mcomp")
+) %dopar% {
+  x <- M3[[i]]$x
+  h <- M3[[i]]$h
+  f <- dotm(y = x, h = h)
+}
 
-out_dstm <- foreach(i = 1:3003, 
-               .packages = c("foreach", "forecast", "Mcomp")) %dopar% {
-                 x <- M3[[i]]$x
-                 h <- M3[[i]]$h
-                 
-                 f <- dstm(y = x, h = h)
-                 
-                 #f
-               }
+out_dstm <- foreach(
+  i = 1:3003, .packages = c("foreach", "forecast", "Mcomp")
+) %dopar% {
+  x <- M3[[i]]$x
+  h <- M3[[i]]$h
+  f <- dstm(y = x, h = h)
+}
 
-out_otm <- foreach(i = 1:3003, 
-               .packages = c("foreach", "forecast", "Mcomp")) %dopar% {
-                 x <- M3[[i]]$x
-                 h <- M3[[i]]$h
-                 
-                 f <- otm(y = x, h = h)
-                 
-                 #f
-               }
+out_otm <- foreach(
+  i = 1:3003, .packages = c("foreach", "forecast", "Mcomp")
+) %dopar% {
+  x <- M3[[i]]$x
+  h <- M3[[i]]$h
+  f <- otm(y = x, h = h)
+}
 
-out_stm <- foreach(i = 1:3003, 
-               .packages = c("foreach", "forecast", "Mcomp")) %dopar% {
-                 x <- M3[[i]]$x
-                 h <- M3[[i]]$h
-                
-                 f <- stm(y = x, h = h)
-                 
-                 #f
-               }
+out_stm <- foreach(
+  i = 1:3003, .packages = c("foreach", "forecast", "Mcomp")
+) %dopar% {
+  x <- M3[[i]]$x
+  h <- M3[[i]]$h
+  f <- stm(y = x, h = h)
+}
 
 stopCluster(cl)
-#fim = Sys.time()
-#fim-inicio
 
 # models out list
 out <- list(out_stm, out_otm, out_dstm, out_dotm)
@@ -67,61 +52,64 @@ results_smape <- c()
 results_mase <- c()
 results_model <- c()
 rn <- c()
+l <- length(out)
 # apply for each model out
-for (mo in 1:length(out)) {
-  
+for (mo in 1:l) {
   outt <- out[[mo]]
-  
-  forec = matrix(NA, nrow=3003, ncol=18)
-  obs = matrix(NA, nrow=3003, ncol=18) #matrix of the out-sample values
+  forec <- matrix(NA, nrow=3003, ncol=18)
+  obs <- matrix(NA, nrow=3003, ncol=18) #matrix of the out-sample values
   meanDiff <- rep(1, 3003)
-  
   for(i in 1:3003){
-    x=M3[[i]]$x
-    h=M3[[i]]$h
-    obs[i,1:h] = M3[[i]]$xx
-    forec[i,1:h] = outt[[i]]$mean
-    meanDiff[i] = mean(abs(diff(x, lag = frequency(x))))
+    x <- M3[[i]]$x
+    h <- M3[[i]]$h
+    obs[i, 1:h] <- M3[[i]]$xx
+    forec[i, 1:h] <- outt[[i]]$mean
+    meanDiff[i] <- mean(abs(diff(x, lag = frequency(x))))
   }
-  
+
   ############## sMAPE ###################
-  sAPE_matrix = errorMetric(obs=obs, forec=forec, type="sAPE", statistic="N")
+  sAPE_matrix = errorMetric(
+    obs = obs, forec = forec, type = "sAPE", statistic = "N"
+  )
+
   #### Yearly ###
-  ye <- round(mean( sAPE_matrix[1:645, 1:6] ), 2)
+  ye <- round(mean(sAPE_matrix[1:645, 1:6]), 2)
+
   #### QUARTERLY ###
-  qe <- round(mean( sAPE_matrix[646:1401, 1:8] ), 2)
+  qe <- round(mean(sAPE_matrix[646:1401, 1:8]), 2)
+
   #### MONTHLY ###
-  me <- round(mean( sAPE_matrix[1402:2829, 1:18] ), 2)
+  me <- round(mean(sAPE_matrix[1402:2829, 1:18]), 2)
+
   #### Other ###
-  ot <- round(mean( sAPE_matrix[2830:3003, 1:8] ), 2)
+  ot <- round(mean(sAPE_matrix[2830:3003, 1:8]), 2)
+
   #### ALL ###
-  all <- round(mean( sAPE_matrix, na.rm=TRUE ), 2)
-  
-  #### ALL 2 ###
-  #sAPE_matrix %>% rowMeans(na.rm=T) %>% mean()
-  
+  all <- round(mean(sAPE_matrix, na.rm = TRUE), 2)
   results_smape <- c(results_smape, ye, qe, me, ot, all)
-  
+
   ############# MASE ######################
-  AE_matrix = errorMetric(obs=obs, forec=forec, type="AE", statistic="N")
-  ASE_matrix=AE_matrix/meanDiff
+  AE_matrix = errorMetric(
+    obs = obs, forec = forec, type = "AE", statistic = "N"
+  )
+  ASE_matrix <- AE_matrix / meanDiff
+
   #### Yearly ###
-  ye <- round(mean( ASE_matrix[1:645, 1:6] ), 2)
+  ye <- round(mean(ASE_matrix[1:645, 1:6]), 2)
+
   #### QUARTERLY ###
-  qe <- round(mean( ASE_matrix[646:1401, 1:8] ), 2)
+  qe <- round(mean(ASE_matrix[646:1401, 1:8]), 2)
+
   #### MONTHLY ###
-  me <- round(mean( ASE_matrix[1402:2829, 1:18] ), 2)
+  me <- round(mean(ASE_matrix[1402:2829, 1:18]), 2)
+
   #### Other ###
-  ot <- round(mean( ASE_matrix[2830:3003, 1:8] ), 2) 
+  ot <- round(mean(ASE_matrix[2830:3003, 1:8]), 2)
+
   #### ALL ###
-  all <- round(mean( ASE_matrix, na.rm=TRUE ), 2)
-  
-  #### ALL 2 ###
-  #ASE_matrix %>% rowMeans(na.rm=T) %>% mean()
-  
+  all <- round(mean(ASE_matrix, na.rm = TRUE ), 2)  
   results_mase <- c(results_mase, ye, qe, me, ot, all)
   ########################################################
-  
   results_model <- c(results_model, rep(tail(outt, 1)[[1]]$method, 5))
   rn <- c(rn, c("Yearly", "Quarterly", "Monthly", "Other", "All"))
 }
