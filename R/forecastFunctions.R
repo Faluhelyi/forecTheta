c05cebbddd9044e9ad784308e76d3b0dbd678d95
Faluhelyi <igor.faluhelyi@outlook.com>
2025-07-06 21:18:21 -0300

    Organiza os arquivos e exporta no NAMESPACE as versões sazonais do OTM E DOTM, além de incluir o Modelo Theta Sazonal

--- R/forecastFunctions.R
+++ R/forecastFunctions.R
@@ -1274,15 +1274,15 @@ seasonal_otm <- function(y, h=5, level=c(80,90,95),
 }
 
 
-seasonal_stm <- function(y, h=5, level=c(80,90,95),
+seasonal_ThetaModel <- function(y, h=5, level=c(80,90,95),
                 s_type="multiplicative", #additive
                 s_test="default",
                 lambda=NULL, par_ini=c(y[1]/2, 0.5, 2.0), estimation=TRUE,
                 lower=c(-1e+10, 0.1, 1.99999), upper=c(1e+10, 0.99, 2.00001),
                 opt.method="Nelder-Mead", xreg=NULL, s=NULL){
 
-  if(!is.ts(y)){ stop("ERROR in seasonal_stm function: y must be an object of time series class."); }
-  if(!is.numeric(h)){	stop("ERROR in seasonal_stm function: h must be a positive integer number.");}
+  if(!is.ts(y)){ stop("ERROR in seasonal_ThetaModel function: y must be an object of time series class."); }
+  if(!is.numeric(h)){	stop("ERROR in seasonal_ThetaModel function: h must be a positive integer number.");}
 
   # Conversion of deprecated argument to new arguments.
   if(!is.null(s)){
@@ -1503,210 +1503,7 @@ bagged_stm <- function(y, h=5, level=c(80,90,95),
 
 ############ Bagged Seasonal Models ############################################
 
-bagged_seasonal_twoTL <- function(y, h, level,
-                         num_bootstrap = 1,   # number of bootstrap replication
-                         bs_bootstrap = NULL, # bootstrap block size
-                         s_type, ## s_type = c("additive","multiplicative","stl")
-                         s_test, ## s_test = c("default","unit_root",TRUE, FALSE)
-                         par_ini, estimation, lower, upper, opt.method, dynamic,
-                         xreg=NULL, s = NULL,
-                         lambda=NULL  ## parameter of Box-Cox transformation
-)
-{
-  if(!is.ts(y)){ stop("ERROR in bagged_seasonal_twoTL function: y must be an object of time series class."); }
-  if(!is.numeric(h)){	stop("ERROR in bagged_seasonal_twoTL function: h must be a positive integer number.");}
-
-  # Conversion of deprecated argument to new arguments.
-  if(!is.null(s)){
-    if(is.logical(s)){
-      s_test = s
-    }else{
-      if (s == "additive"){
-        s_type = "additive"
-      }
-    }
-  }
-
-  fq <- frequency(y)
-
-  run_s_decomp = seasonal_test(y, s_test)
-  if( fq < 3 ){
-    s_type="None";s_type="None";
-  }
-
-  if( run_s_decomp ){
-
-    s_type = match.arg(arg=s_type, choices=c("additive","multiplicative","stl"))
-
-    if( s_type == "multiplicative" ){
-      s_ini <- initial_seasonal_component(y=y, m=fq, type = s_type)
-
-      par_ini <- c(par_ini, 0.5, s_ini)
-      lower <- c(lower, 0.1, rep(-1e+10, fq))
-      upper <- c(upper, 0.99, rep(1e+10, fq))
-    }else{
-      s_ini <- initial_seasonal_component(y=y, m=fq, type = s_type)
-
-      par_ini <- c(par_ini, 0.5, s_ini)
-      lower <- c(lower, 0.1, rep(-1e+10, fq))
-      upper <- c(upper, 0.99, rep(1e+10, fq))
-    }
-  }
-  #else{
-  #  par_ini <- c(par_ini, 0.0, rep(0.0, fq))
-  #  lower <- c(lower, -1e-10, rep(-1e-10, fq))
-  #  upper <- c(upper, 1e-10, rep(1e-10, fq))
-  #}
-
-  nSample = 10000
-  y_bagged = list(y)
-  if(num_bootstrap > 1 ){
-    y_bagged = bld.mbb.bootstrap(y, num=num_bootstrap)
-    nSample = max( nSample %/% num_bootstrap, 1)
-  }
-
-  if(num_bootstrap>1 && is.null(level)){level=c(80,90,95);} ## important for running bagging
-
-  y_i = NULL
-  models = foreach(y_i = y_bagged) %do% {
-
-    fit = seasonal_twoTL(y=y_i, h=h, level=level, s_type=s_type, s_test=s_test,
-                par_ini=par_ini, estimation=estimation, lower=lower, upper=upper,
-                opt.method=opt.method, dynamic=dynamic, xreg=xreg, lambda=lambda,
-                nSample=nSample)
-
-    fit
-  }
-
-  out = models[[1]]
-  out$num_bootstrap = num_bootstrap
-
-  if( num_bootstrap == 1 ){
-    out$matForec.sample = NULL
-    return(out);
-  }
-
-  # bs_means = foreach(fit = models, .combine=cbind) %do% { fit$mean; }
-  # colnames(bs_means) = paste0("m", 1:ncol(bs_means) )
-  # bs_means_mean = ts(rowMeans(bs_means), start=start(bs_means), frequency = frequency(y))
-  # bs_means_median = apply(X=bs_means, MARGIN=1, FUN=quantile,  probs=0.5)
-  # bs_means_median = ts(bs_means_median, start=start(bs_means), frequency = frequency(y))
-  # out$bs_means_mean = bs_means_mean
-  # out$bs_means_median = bs_means_median
-
-  bs_strapolations = foreach(fit = models, .combine=cbind) %do% { fit$matForec.sample; }
-  colnames(bs_strapolations) = paste0("m", 1:ncol(bs_strapolations) )
-  bs_st_mean = ts(rowMeans(bs_strapolations), start=start(out$mean), frequency = frequency(y))
-  bs_st_median = apply(X=bs_strapolations, MARGIN=1, FUN=quantile,  probs=0.5)
-  bs_st_median = ts(bs_st_median, start=start(out$mean), frequency = frequency(y))
-
-  out$mean = bs_st_mean
-  out$median =  bs_st_median
-
-  quantiles = bootstrap_quantiles( bs_series=bs_strapolations, level=level )
-  quantiles = ts( quantiles, start = end(y) + c(0, 1), frequency = frequency(y))
-  nn = length(level)
-  out$lower = quantiles[, 2*(1:nn)-1, drop=F]
-  out$upper = quantiles[, 2*(1:nn), drop=F]
-
-  out$matForec.sample = NULL
-  out$tests = NULL
-
-  return(out)
-}
-
-
-
-
-bagged_seasonal_dotm <- function(y, h=5, level=c(80,90,95),
-                        num_bootstrap = 100,   # number of bootstrap replication
-                        bs_bootstrap = NULL, # bootstrap block size
-                        s_type="multiplicative",
-                        s_test="default",
-                        lambda=NULL, par_ini=c(y[1]/2, 0.5, 2), estimation=TRUE,
-                        lower=c(-1e+10, 0.1, 1.0), upper=c(1e+10, 0.99, 1e+10),
-                        opt.method="Nelder-Mead", xreg=NULL, s = NULL ){
-
-  out =  bagged_seasonal_twoTL( y=y, h=h, level=level,
-                       num_bootstrap = num_bootstrap, bs_bootstrap = bs_bootstrap,
-                       s_type=s_type, s_test=s_test, par_ini=par_ini,
-                       estimation=estimation, lower=lower, upper=upper, opt.method=opt.method,
-                       dynamic=TRUE, xreg=xreg, lambda=lambda)
-
-  out$method = "Dynamic Seasonal Optimised Theta Model"
-  if(out$num_bootstrap > 1){ out$method = paste("Bagged", out$method); }
-
-  return(out)
-}
-
-bagged_seasonal_dstm <- function(y, h=5, level=c(80,90,95),
-                        num_bootstrap = 100,   # number of bootstrap replication
-                        bs_bootstrap = NULL, # bootstrap block size
-                        s_type="multiplicative", s_test="default",
-                        lambda=NULL, par_ini=c(y[1]/2, 0.5, 2.0),estimation=TRUE,
-                        lower=c(-1e+10, 0.1, 1.99999), upper=c(1e+10, 0.99, 2.00001),
-                        opt.method="Nelder-Mead", xreg=NULL, s = NULL ){
-
-  out =  bagged_seasonal_twoTL( y=y, h=h, level=level,
-                       num_bootstrap = num_bootstrap, bs_bootstrap = bs_bootstrap,
-                       s_type=s_type, s_test=s_test, par_ini=par_ini,
-                       estimation=estimation, lower=lower, upper=upper,
-                       opt.method=opt.method, dynamic=TRUE, xreg=xreg, lambda=lambda)
-
-  out$method = "Dynamic Seasonal Standard Theta Model"
-  if(out$num_bootstrap > 1){ out$method = paste("Bagged", out$method); }
-  out$par = as.matrix(out$par[setdiff(rownames(out$par), "theta"), ])
-  colnames(out$par) = 'MLE'
-
-  return(out)
-}
-
-
-bagged_seasonal_otm <- function(y, h=5, level=c(80,90,95),
-                       num_bootstrap = 100,   # number of bootstrap replication
-                       bs_bootstrap = NULL, # bootstrap block size
-                       s_type="multiplicative", s_test="default",
-                       lambda=NULL, par_ini=c(y[1]/2, 0.5, 2.0), estimation=TRUE,
-                       lower=c(-1e+10, 0.1, 1.0), upper=c(1e+10, 0.99, 1e+10),
-                       opt.method="Nelder-Mead", xreg=NULL, s = NULL ){
-
-  out = bagged_seasonal_twoTL( y=y, h=h, level=level,
-                      num_bootstrap = num_bootstrap, bs_bootstrap = bs_bootstrap,
-                      s_type=s_type, s_test=s_test,
-                      par_ini=par_ini, estimation=estimation, lower=lower,
-                      upper=upper, opt.method=opt.method, dynamic=FALSE, xreg=xreg,
-                      lambda=lambda)
-
-  out$method = "Seasonal Optimised Theta Model"
-  if(out$num_bootstrap > 1){ out$method = paste("Bagged", out$method); }
-
-  return(out)
-}
-
-bagged_seasonal_stm <- function(y, h=5, level=c(80,90,95),
-                       num_bootstrap = 100,   # number of bootstrap replication
-                       bs_bootstrap = NULL, # bootstrap block size
-                       s_type="multiplicative", s_test="default",
-                       lambda=NULL, par_ini=c(y[1]/2, 0.5, 2.0), estimation=TRUE,
-                       lower=c(-1e+10, 0.1, 1.99999), upper=c(1e+10, 0.99, 2.00001),
-                       opt.method="Nelder-Mead", xreg=NULL, s = NULL ){
-
-  out = bagged_seasonal_twoTL( y=y, h=h, level=level,
-                      num_bootstrap = num_bootstrap, bs_bootstrap = bs_bootstrap,
-                      s_type=s_type, s_test=s_test,
-                      par_ini=par_ini, estimation=estimation,
-                      lower=lower, upper=upper,
-                      opt.method=opt.method, dynamic=FALSE, xreg=xreg, lambda=lambda)
-
-  out$method = "Seasonal Standard Theta Model"
-  if(out$num_bootstrap > 1){ out$method = paste("Bagged", out$method); }
-  out$par = as.matrix(out$par[setdiff(rownames(out$par), "theta"), ])
-  colnames(out$par) = 'MLE'
-
-  return(out)
-}
-
-
+######################################################
 
 ###################  SES  ######################################################
 expSmoot <- function(y, h=5, ell0=NULL, alpha=NULL, lower = c(-1e+10, 0.1),
--- benchmarks/m3_mase_smape.R
+++ benchmarks/m3_mase_smape.R
@@ -1,142 +1,73 @@
-# benchmark/teste_modelos_theta.R
+#devtools::install()
 
-# =====================================
-# Benchmark: Avaliação dos modelos Theta
-# =====================================
-
-# Pacotes necessários
+library(forecTheta)
+library(forecast)
 library(Mcomp)
+library(foreach)
 library(parallel)
 library(doParallel)
-library(iterators)
-library(forecast)
 
-# Carrega o código do pacote localmente
-devtools::load_all(".")
+data(M3)
 
-# =====================
-# Configuração do cluster
-# =====================
+inicio = Sys.time()
+
 cl <- makeCluster(detectCores(logical = FALSE))
 registerDoParallel(cl)
 
-# Carrega funções do pacote em cada nó
-clusterEvalQ(cl, devtools::load_all("."))
-#clusterExport(cl, c("dotm", "dstm", "otm", "stm"))
-clusterExport(cl, c("seasonal_dotm", "seasonal_dstm", "seasonal_otm",
-                    "seasonal_stm"))
-
-#clusterExport(cl, c("bagged_seasonal_dotm", "bagged_seasonal_dstm",
-#                    "bagged_seasonal_otm", "bagged_seasonal_stm"))
-
-# =====================
-# Execução dos modelos em paralelo
-# =====================
-model_fits <- list()
-
-model_fits$dotm <- foreach(
-  i = 1:3003, .packages = c("forecast", "Mcomp")
-) %dopar% {
-  x <- M3[[i]]$x
-  h <- M3[[i]]$h
-  #bagged_seasonal_dotm(y = x, h = h)
-  seasonal_dotm(y = x, h = h)
-  #dotm(y = x, h = h)
+out = foreach(i = 1:3003, .packages = c("forecast", "forecTheta")) %dopar% {
+  x = M3[[i]]$x
+  h = M3[[i]]$h
+  f = seasonal_ThetaModel(y = x, h = h)
+  f
 }
-
-#model_fits$dstm <- foreach(
-#  i = 1:3003, .packages = c("forecast", "Mcomp")
-#) %dopar% {
-#  x <- M3[[i]]$x
-#  h <- M3[[i]]$h
-#  seasonal_dstm(y = x, h = h, s_type = "additive", s_test = "unit_root")
-#  #dstm(y = x, h = h, s_type = "additive", s_test = "unit_root")
-#}
-
-model_fits$otm <- foreach(
-  i = 1:3003, .packages = c("forecast", "Mcomp")
-) %dopar% {
-  x <- M3[[i]]$x
-  h <- M3[[i]]$h
-  #bagged_seasonal_otm(y = x, h = h, s_type = "multiplicative", s_test = "unit_root")
-  seasonal_otm(y = x, h = h)
-  #otm(y = x, h = h)
-}
-
-#model_fits$stm <- foreach(
-#  i = 1:3003, .packages = c("forecast", "Mcomp")
-#) %dopar% {
-#  x <- M3[[i]]$x
-#  h <- M3[[i]]$h
-#  seasonal_stm(y = x, h = h, s_type = "additive", s_test = "unit_root")
-#  #stm(y = x, h = h, s_type = "additive", s_test = "unit_root")
-#}
 
 stopCluster(cl)
 
-# =====================
-# Avaliação dos resultados
-# =====================
+fim = Sys.time()
+fim - inicio
 
-calculate_errors <- function(outt, model_name) {
-  forec <- matrix(NA, nrow = 3003, ncol = 18)
-  obs <- matrix(NA, nrow = 3003, ncol = 18)
-  meanDiff <- rep(1, 3003)
 
-  for (i in 1:3003) {
-    x <- M3[[i]]$x
-    h <- M3[[i]]$h
-    obs[i, 1:h] <- M3[[i]]$xx
-    forec[i, 1:h] <- outt[[i]]$mean
-    meanDiff[i] <- mean(abs(diff(x, lag = frequency(x))))
-  }
+forec = matrix(NA, nrow=3003, ncol=18)
+obs = matrix(NA, nrow=3003, ncol=18) #matrix of the out-sample values
+meanDiff <- rep(1, 3003)
 
-  # sMAPE
-  sAPE_matrix <- errorMetric(obs = obs, forec = forec, type = "sAPE", statistic = "N")
-  smape_vals <- c(
-    Yearly = mean(sAPE_matrix[1:645, 1:6]),
-    Quarterly = mean(sAPE_matrix[646:1401, 1:8]),
-    Monthly = mean(sAPE_matrix[1402:2829, 1:18]),
-    Other = mean(sAPE_matrix[2830:3003, 1:8]),
-    All = mean(sAPE_matrix, na.rm = TRUE)
-  )
-
-  # MASE
-  AE_matrix <- errorMetric(obs = obs, forec = forec, type = "AE", statistic = "N")
-  ASE_matrix <- AE_matrix / meanDiff
-  mase_vals <- c(
-    Yearly = mean(ASE_matrix[1:645, 1:6]),
-    Quarterly = mean(ASE_matrix[646:1401, 1:8]),
-    Monthly = mean(ASE_matrix[1402:2829, 1:18]),
-    Other = mean(ASE_matrix[2830:3003, 1:8]),
-    All = mean(ASE_matrix, na.rm = TRUE)
-  )
-
-  # Retorna os dois vetores em formato data.frame
-  data.frame(
-    Period = names(smape_vals),
-    sMAPE = round(smape_vals, 2),
-    MASE = round(mase_vals, 2),
-    Model = model_name
-  )
+for(i in 1:3003){
+  x=M3[[i]]$x
+  h=M3[[i]]$h
+  obs[i,1:h] = M3[[i]]$xx
+  forec[i,1:h] = out[[i]]$mean
+  meanDiff[i] = mean(abs(diff(x, lag = frequency(x))))
 }
 
-# Aplicar para todos os modelos
-results_list <- list(
-  #calculate_errors(model_fits$stm, "Seasonal Standard Theta Model"),
-  calculate_errors(model_fits$otm, "SOTM"),
-  #calculate_errors(model_fits$dstm, "Dynamic Seasonal Standard Theta Model"),
-  calculate_errors(model_fits$dotm, "Dynamic SOTM")
-)
+############## sMAPE ###################
+sAPE_matrix = errorMetric(obs=obs, forec=forec, type="sAPE", statistic="N")
+#### Yearly ###
+mean( sAPE_matrix[1:645, 1:6] ) %>% round(2)
+#### QUARTERLY ###
+mean( sAPE_matrix[646:1401, 1:8] ) %>% round(2)
+#### MONTHLY ###
+mean( sAPE_matrix[1402:2829, 1:18] ) %>% round(2)
+#### Other ###
+mean( sAPE_matrix[2830:3003, 1:8] ) %>% round(2)
+#### ALL ###
+mean( sAPE_matrix, na.rm=TRUE ) %>% round(2)
+#### ALL 2 ###
+sAPE_matrix %>% rowMeans(na.rm=T) %>% mean() %>% round(2)
 
-# Combinar resultados em um único data.frame
-results <- do.call(rbind, results_list)
-
-# =====================
-# Exibir resultado final
-# =====================
-print(results)
-
-# Opcional: salvar em CSV
-write.csv(results, "benchmarks/resultados_theta_sazonal_multiplicative.csv", row.names = FALSE)
+#
+############# MASE ######################
+AE_matrix = errorMetric(obs=obs, forec=forec, type="AE", statistic="N")
+ASE_matrix=AE_matrix/meanDiff
+#### Yearly ###
+mean( ASE_matrix[1:645, 1:6] ) %>% round(2)
+#### QUARTERLY ###
+mean( ASE_matrix[646:1401, 1:8] ) %>% round(2)
+#### MONTHLY ###
+mean( ASE_matrix[1402:2829, 1:18] ) %>% round(2)
+#### Other ###
+mean( ASE_matrix[2830:3003, 1:8] ) %>% round(2)
+#### ALL ###
+mean( ASE_matrix, na.rm=TRUE ) %>% round(2)
+#### ALL 2 ###
+ASE_matrix %>% rowMeans(na.rm=T) %>% mean() %>% round(2)
 
